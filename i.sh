75bc051502f06f55cf7c7e78e9b61978901e8d67
shirazih <121781682+shirazih@users.noreply.github.com>
2023-01-04 21:19:47 -0800

    Update i.sh

--- i.sh
+++ i.sh
@@ -77,22 +77,22 @@ ufw default allow outgoing 2>> 2.log 1>> 1.log
 ufw default deny incoming 2>> 2.log 1>> 1.log
 ufw allow ssh 2>> 2.log 1>> 1.log
 ufw allow 80 2>> 2.log 1>> 1.log
-ufw allow 443 2>> 2.log 1>> 1.log
+ufw allow $TPORT 2>> 2.log 1>> 1.log
 ufw deny $MYPORT 2>> 2.log 1>> 1.log
 echo "y" | ufw enable 2>> 2.log 1>> 1.log
 
 echo -e "\n$HC+$NC Creating the config..."
 
 curl --cookie-jar cookies.txt "http://$MYDOMAIN:$MYPORT/login" --data-raw "username=$MYUSER&password=$MYPASS" 2>> 2.log 1>> 1.log
-curl --cookie cookies.txt "http://$MYDOMAIN:$MYPORT/xui/inbound/add" --data-raw "up=0&down=0&total=0&remark=$TNAME&enable=true&expiryTime=0&listen=&port=443&protocol=trojan&settings=%7B%22clients%22%3A%5B%7B%22password%22%3A%22$TPASS%22%2C%22flow%22%3A%22xtls-rprx-direct%22%7D%5D%2C%22fallbacks%22%3A%5B%5D%7D&streamSettings=%7B%22network%22%3A%22tcp%22%2C%22security%22%3A%22tls%22%2C%22tlsSettings%22%3A%7B%22serverName%22%3A%22$MYDOMAIN%22%2C%22certificates%22%3A%5B%7B%22certificateFile%22%3A%22%2Fetc%2Fletsencrypt%2Flive%2F$MYDOMAIN%2Ffullchain.pem%22%2C%22keyFile%22%3A%22%2Fetc%2Fletsencrypt%2Flive%2F$MYDOMAIN%2Fprivkey.pem%22%7D%5D%7D%2C%22tcpSettings%22%3A%7B%22header%22%3A%7B%22type%22%3A%22none%22%7D%7D%7D&sniffing=%7B%22enabled%22%3Atrue%2C%22destOverride%22%3A%5B%22http%22%2C%22tls%22%5D%7D" 2>> 2.log 1>> 1.log
+curl --cookie cookies.txt "http://$MYDOMAIN:$MYPORT/xui/inbound/add" --data-raw "up=0&down=0&total=0&remark=$TNAME&enable=true&expiryTime=0&listen=&port=$TPORT&protocol=trojan&settings=%7B%22clients%22%3A%5B%7B%22password%22%3A%22$TPASS%22%2C%22flow%22%3A%22xtls-rprx-direct%22%7D%5D%2C%22fallbacks%22%3A%5B%5D%7D&streamSettings=%7B%22network%22%3A%22tcp%22%2C%22security%22%3A%22tls%22%2C%22tlsSettings%22%3A%7B%22serverName%22%3A%22$MYDOMAIN%22%2C%22certificates%22%3A%5B%7B%22certificateFile%22%3A%22%2Fetc%2Fletsencrypt%2Flive%2F$MYDOMAIN%2Ffullchain.pem%22%2C%22keyFile%22%3A%22%2Fetc%2Fletsencrypt%2Flive%2F$MYDOMAIN%2Fprivkey.pem%22%7D%5D%7D%2C%22tcpSettings%22%3A%7B%22header%22%3A%7B%22type%22%3A%22none%22%7D%7D%7D&sniffing=%7B%22enabled%22%3Atrue%2C%22destOverride%22%3A%5B%22http%22%2C%22tls%22%5D%7D" 2>> 2.log 1>> 1.log
 
 
-echo -e "PANEL: http://$MYDOMAIN:$MYPORT\n" > panel.txt
-echo -e "USER: $MYUSER\n" >> panel.txt
-echo -e "PASS: $MYPASS\n" >> panel.txt
-echo -e "Config: trojan://$TPASS@$MYDOMAIN:$TPORT#$TNAME\n" >> panel.txt
-echo -e "Public Key:  /etc/letsencrypt/live/$MYDOMAIN/fullchain.pem\n" >> panel.txt
-echo -e "Private Key: /etc/letsencrypt/live/$MYDOMAIN/privkey.pem\n" >> panel.txt
+echo -e "\nPANEL: http://$MYDOMAIN:$MYPORT" > panel.txt
+echo -e "\nUSER: $MYUSER" >> panel.txt
+echo -e "\nPASS: $MYPASS" >> panel.txt
+echo -e "\nConfig: trojan://$TPASS@$MYDOMAIN:$TPORT#$TNAME" >> panel.txt
+echo -e "\nPublic Key:  /etc/letsencrypt/live/$MYDOMAIN/fullchain.pem" >> panel.txt
+echo -e "\nPrivate Key: /etc/letsencrypt/live/$MYDOMAIN/privkey.pem" >> panel.txt
 
 echo -e "\n \e[1;30;106m[\xE2\x9C\x94]$NC - \e[1m Proxy Config: $HC trojan://$TPASS@$MYDOMAIN:$TPORT#$TNAME $NC\n\n \e[2m *** Good luck. *** $NC  \e[1;30;46m(⌐■_■)$NC \n\n"
 rm -f "$FLAGFILE"
